2ab27756fe41da66aee3f223463ae35f650aa071
Keith <keithangjy96@gmail.com>
2025-04-09 11:39:08 +0800

    fixed scrollbar issue

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "../components/shared/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "../components/theme-provider";
 import { Toaster } from "sonner";
+import { WebSocketProvider } from "@/lib/contexts/WebSocketContext";
 
 const poppins = Poppins({
 	variable: "--font-poppins",
@@ -15,11 +16,11 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
 	title: "Quest Research",
-	description:   "LLM based autonomous agent that conducts local and web research on any topic and generates a comprehensive report with citations."
-	,
+	description:
+		"LLM based autonomous agent that conducts local and web research on any topic and generates a comprehensive report with citations.",
 	icons: {
 		icon: "../images/quest-logo.png",
-	  },
+	},
 };
 
 export default function RootLayout({
@@ -33,24 +34,26 @@ export default function RootLayout({
 				<body
 					className={`${poppins.variable} antialiased vsc-initialized`}
 				>
-					<ThemeProvider
-						attribute="class"
-						enableSystem
-						defaultTheme="system"
-					>
-						{/* Dark mode background */}
-						<div className="fixed inset-0 -z-10 h-full w-full dark:block hidden [background:radial-gradient(125%_125%_at_50%_10%,#1d1d1d_40%,#5f2bed_100%)]"></div>
+					<WebSocketProvider>
+						<ThemeProvider
+							attribute="class"
+							enableSystem
+							defaultTheme="system"
+						>
+							{/* Dark mode background */}
+							<div className="fixed inset-0 -z-10 h-full w-full dark:block hidden [background:radial-gradient(125%_125%_at_50%_10%,#1d1d1d_40%,#5f2bed_100%)]"></div>
 
-						{/* Light mode background */}
-						<div className="fixed inset-0 -z-10 h-full w-full dark:hidden block bg-white [background:radial-gradient(125%_125%_at_50%_10%,#d4d4d4_40%,#63e_100%)]"></div>
+							{/* Light mode background */}
+							<div className="fixed inset-0 -z-10 h-full w-full dark:hidden block bg-white [background:radial-gradient(125%_125%_at_50%_10%,#d4d4d4_40%,#63e_100%)]"></div>
 
-						<div className="relative flex h-screen w-full flex-col">
-							<Header />
-							<main className="flex-1 overflow-y-auto">{children}</main>
-							{/* <Footer /> */}
-						</div>
-						<Toaster position="bottom-right" duration={5000} />
-					</ThemeProvider>
+							<div className="relative flex h-screen w-full flex-col">
+								<Header />
+								<main className="flex-1">{children}</main>
+								{/* <Footer /> */}
+							</div>
+							<Toaster position="bottom-right" duration={5000} />
+						</ThemeProvider>
+					</WebSocketProvider>
 				</body>
 			</html>
 		</ClerkProvider>
